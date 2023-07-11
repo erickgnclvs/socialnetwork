d7c4e0fcc8ad64815f249cf60b9a55fb796057b7
erick <erickgoncalves5@hotmail.com>
2023-07-11 15:18:53 -0300

    Crop image to be always square

--- src/main/java/erick/projects/socialnetwork/controller/ImageController.java
+++ src/main/java/erick/projects/socialnetwork/controller/ImageController.java
@@ -3,7 +3,7 @@ package erick.projects.socialnetwork.controller;
 import erick.projects.socialnetwork.model.Image;
 import erick.projects.socialnetwork.repository.ImageRepository;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Controller
@@ -24,18 +27,64 @@ public class ImageController {
         this.imageRepository = imageRepository;
     }
 
+//    @GetMapping("/images/{imageId}")
+//    public ResponseEntity<byte[]> getImage(@PathVariable Long imageId) {
+//        // Find the image in the database
+//        Image img = imageRepository.findById(imageId).orElseThrow();
+//        // Set the response headers
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.parseMediaType(img.getImageType()));
+//        headers.setContentLength(img.getImage().length);
+//
+//        // Return the image as a byte array
+//        return new ResponseEntity<>(img.getImage(), headers, HttpStatus.OK);
+//    }
+
     @GetMapping("/images/{imageId}")
     public ResponseEntity<byte[]> getImage(@PathVariable Long imageId) {
         // Find the image in the database
         Image img = imageRepository.findById(imageId).orElseThrow();
-        // Set the response headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(img.getImageType()));
-        headers.setContentLength(img.getImage().length);
 
-        // Return the image as a byte array
-        return new ResponseEntity<>(img.getImage(), headers, HttpStatus.OK);
+        try {
+            // Convert the image byte array to a BufferedImage
+            ByteArrayInputStream bais = new ByteArrayInputStream(img.getImage());
+            BufferedImage originalImage = ImageIO.read(bais);
+
+            // Calculate the size of the square
+            int squareSize = Math.min(originalImage.getWidth(), originalImage.getHeight());
+
+            // Calculate the coordinates of the center of the image
+            int xCenter = originalImage.getWidth() / 2;
+            int yCenter = originalImage.getHeight() / 2;
+
+            // Calculate the top left coordinates of the square
+            int x = xCenter - (squareSize / 2);
+            int y = yCenter - (squareSize / 2);
+
+            // Crop the image to a square
+            BufferedImage croppedImage = originalImage.getSubimage(x, y, squareSize, squareSize);
+
+            // Convert the cropped image to a byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(croppedImage, img.getImageType().split("/")[1], baos);
+            byte[] imageBytes = baos.toByteArray();
+
+            // Set the response headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(img.getImageType()));
+            headers.setContentLength(imageBytes.length);
+
+            // Return the cropped image as a byte array
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            // Handle any errors
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+
+
 
     @GetMapping("/images/default")
     public ResponseEntity<byte[]> getDefaultImage() {
