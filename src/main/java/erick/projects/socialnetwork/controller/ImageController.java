2c81b81175b5fc9e0e0bbbd0d5d4008e3366fd38
erick <erickgoncalves5@hotmail.com>
2023-07-11 15:04:36 -0300

    Implement default profile picture

--- src/main/java/erick/projects/socialnetwork/controller/ImageController.java
+++ src/main/java/erick/projects/socialnetwork/controller/ImageController.java
@@ -2,7 +2,10 @@ package erick.projects.socialnetwork.controller;
 
 import erick.projects.socialnetwork.model.Image;
 import erick.projects.socialnetwork.repository.ImageRepository;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.IOException;
 
 @Controller
 public class ImageController {
@@ -31,5 +36,25 @@ public class ImageController {
         // Return the image as a byte array
         return new ResponseEntity<>(img.getImage(), headers, HttpStatus.OK);
     }
+
+    @GetMapping("/images/default")
+    public ResponseEntity<byte[]> getDefaultImage() {
+        // Load the default image from the classpath
+        Resource resource = new ClassPathResource("/static/images/default-profile-picture.png");
+        try {
+            // Set the response headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            headers.setContentLength(resource.contentLength());
+
+            // Return the image as a byte array
+            return new ResponseEntity<>(IOUtils.toByteArray(resource.getInputStream()), headers, HttpStatus.OK);
+        } catch (IOException e) {
+            // Handle any errors
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
